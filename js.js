fe16dc28659b04637690ee190531c7103bf5c6d0
mmperez12 <mpereza2@fcpn.edu.bo>
2024-06-06 15:59:43 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+/*Menu desplegable*/
+function toggleContainer() {
+  var container = document.getElementById("container");
+  if (container.style.display === "none") {
+    container.style.display = "block";
+  } else {
+    container.style.display = "none";
+  }
+}
